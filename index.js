6634e15ba3f600ae80288545c1c152125068db1d
Tomas <bivainis.tomas@gmail.com>
2021-12-05 11:50:58 +0200

    api setup and ready to go

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 8081;
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+}));
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
